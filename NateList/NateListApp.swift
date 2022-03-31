2ed2cbd88a82093b94448bcfc8a5f790c2bd6a92
kiranjith.kolankata <kiranjith.kolankata@deltatre.com>
2022-03-31 08:36:07 +0100

    Initial Commit

---
+++ NateList/NateListApp.swift
@@ -0,0 +1,17 @@
+//
+//  NateListApp.swift
+//  NateList
+//
+//  Created by Kiranjith Kolankata on 31/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct NateListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
